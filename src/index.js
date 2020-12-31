6797e76fb48fa5f7886b437982dc36fe5cbbb86f
Rami Reddy <email>
2020-12-31 17:37:31 -0500

    installed react dev deps + eslint + prettier

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-import text from './text'
-console.log(` Hello world! + ${text()}`)
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World!</h1>;
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,29 @@
-const path = require('path')
-const { mainModule } = require('process')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const { main } = require('process')
 
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		path: path.join(__dirname, 'dist'),
-		filename: 'app.js',
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				loader: 'babel-loader',
-				exclude: /node_modules/,
-				options: {
-					presets: ['@babel/preset-env'],
-				},
-			},
-		],
-	},
-}
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+  ],
+};
